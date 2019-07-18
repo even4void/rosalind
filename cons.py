7f7297ba4bbf57931bc7a5985ba28bf805537e20
chl <chl@aliquote.org>
2019-07-18 14:09:42 +0200

    Add problem 10

---
+++ cons.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Problem 10
+# A consensus string c is a string of length n formed from our collection
+# by taking the most common symbol at each position; the j th symbol of c
+# therefore corresponds to the symbol having the maximum value in the j-th
+# column of the profile matrix. Of course, there may be more than one most
+# common symbol, leading to multiple possible consensus strings.
+# Given: A collection of at most 10 DNA strings of equal length (at most 1
+# kbp) in FASTA format.
+# Return: A consensus string and profile matrix for the collection. (If
+# several possible consensus strings exist, then you may return any one of
+# them.)
