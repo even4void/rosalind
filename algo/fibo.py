b407307f9d70906d8604b7628e5935be9c2bb03a
chl <chl@aliquote.org>
2019-08-21 16:11:57 +0200

    Reorganize directories + Add new track

---
+++ algo/fibo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import functools
+
+
+# Note that we use "built-in" memoization, although for those kind of numbers,
+# this should not matter. The Lru_cache decorator is well described on SO
+# (https://stackoverflow.com/a/49883466). An alternative recursive solution
+# is also provided on SO (https://stackoverflow.com/a/38445336).
+
+@functools.lru_cache(None)
+def fib(n):
+    """
+    >>> fib(6)
+    8
+    """
+    if n < 2:
+        return n
+    return fib(n-1) + fib(n-2)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
+    print(fib(23))
