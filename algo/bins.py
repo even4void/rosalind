3057f2a01cd26e848f56c1badf1b56d9aac0c1e2
chl <chl@aliquote.org>
2019-08-25 18:55:45 +0200

    Add doctest and true test data

--- algo/bins.py
+++ algo/bins.py
@@ -2,22 +2,31 @@
 
 
 def bins(lst, start, end, elt):
-    """Binary search assuming lst is sorted (recursive version)."""
+    """
+    >>> array = [10, 20, 30, 40, 50]
+    >>> idx = [40, 10, 35, 15, 40, 20]
+    >>> print([bins(array, 0, len(array)-1, x) for x in idx])
+    [4, 1, -1, -1, 4, 2]
+    """
     mid = (start + end) // 2
     if start > end:
         return -1
     if elt == lst[mid]:
-        # simulate 1-based indexing
+        # simulate 1-based indexing for Rasalind
         return mid+1
     elif elt < lst[mid]:
         return bins(lst, start, mid-1, elt)
     else:
-        # elt < lst[mid]
+        # elt > lst[mid]
         return bins(lst, mid+1, end, elt)
 
 
-# See bins.txt for real test data
-array = [10, 20, 30, 40, 50]
-idx = [40, 10, 35, 15, 40, 20]
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
 
-print([bins(array, 0, len(array)-1, x) for x in idx])
+    with open('bins.txt') as f:
+        for line in f:
+            exec(line)
+
+    print([bins(array, 0, len(array)-1, x) for x in idx])
