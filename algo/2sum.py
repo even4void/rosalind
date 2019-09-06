e0dc50f99120cef6e3c0dd8dceb16d22968b284d
chl <chl@aliquote.org>
2019-09-06 15:18:39 +0200

    Add 2SUM

--- algo/2sum.py
+++ algo/2sum.py
@@ -1,31 +1,20 @@
 #!/usr/bin/env python3
 
-from collections import defaultdict
 
-
-def two_sum(lst, x):
-    '''Returns [i,j] such that a[i] + a[j] = x if, otherwise -1.'''
-    items = defaultdict(set)
-
-    for i, e in enumerate(lst):
-        items[e].add(i)
-
-    for e in items:
-        if x - e in items:
-            if x - e == e and len(items[e]) > 1:
-                return sorted([items[e].pop()+1, items[e].pop()+1])
-            else:
-                return sorted([items[e].pop()+1, items[x-e].pop()+1])
-
-    return -1
+def two_sum(lst):
+    lst = list(lst)
+    for t in range(len(lst)):
+        if -lst[t] in lst[t+1:]:
+            for s in range(t+1, len(lst)):
+                if lst[t] == -lst[s]:
+                    return [t+1, s+1]
+    return [-1]
 
 
 if __name__ == '__main__':
     with open('2sum.txt') as f:
-        k, n = map(int, f.readline().rstrip().split())
+        next(f)
         lst = [map(int, line.rstrip().split()) for line in f]
 
-    values = [two_sum(x, 0) for x in lst]
-    values = [str(x) if type(x) is int
-              else ' '.join(map(str, x)) for x in values]
-    print('\n'.join(values))
+    for elt in lst:
+        print(" ".join(map(str, two_sum(elt))))
