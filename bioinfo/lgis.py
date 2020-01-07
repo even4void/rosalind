08c07734de051eddea20e6c3ed442683979a9c63
chl <chl@aliquote.org>
2020-01-07 16:43:38 +0100

    Replace code not working

--- bioinfo/lgis.py
+++ bioinfo/lgis.py
@@ -2,42 +2,42 @@
 
 # NOTE: See also
 # - https://stackoverflow.com/q/3992697
-# - https://www.algorithmist.com/index.php/Longest_Increasing_Subsequence
 
 
-def lis(xs):
-    """Rosetta code: https://is.gd/IZbn7I."""
-    N = len(xs)
-    P = [0] * N
-    M = [0] * (N + 1)
-    L = 0
-    for i in range(N):
-        lo = 1
-        hi = L
-        while lo <= hi:
-            mid = (lo + hi) // 2
-            if xs[M[mid]] < xs[i]:
-                lo = mid + 1
-            else:
-                hi = mid - 1
-        newL = lo
-        P[i] = M[newL - 1]
-        M[newL] = i
-        if newL > L:
-            L = newL
-    S = []
-    k = M[L]
-    for i in range(L - 1, -1, -1):
-        S.append(xs[k])
-        k = P[k]
-    return S[::-1]
+def lseq(n, seq, op):
+    """ mattdoug604/Rosalind """
+    q = [0] * n
+    p = [-1] * n
+
+    for i in range(n):
+        maxLen = 0
+        for j in range(i):
+            if op == '>':
+                check = seq[i] > seq[j]
+            elif op == '<':
+                check = seq[i] < seq[j]
+            if check is True:
+                if q[j] > maxLen:
+                    maxLen = q[j]
+                    p[i] = j
+
+        q[i] = maxLen + 1
+
+    idx = q.index(max(q))
+    ls = []
+    while(idx != -1):
+        ls = [seq[idx]] + ls
+        idx = p[idx]
+
+    return ls
 
 
 lst = open("lgis.txt").readlines()
 n, p = [x.strip() for x in lst]
-p = p.split()
+n = int(n)
+p = [int(i) for i in p.split()]
 
-incr = lis(p)
-decr = lis(p[::-1])[::-1]
+incr = lseq(n, p, '>')
+decr = lseq(n, p, '<')
 
-print("%s\n%s" % (" ".join(incr), " ".join(decr)))
+print("%s\n%s" % (" ".join(map(str, incr)), " ".join(map(str, decr))))
