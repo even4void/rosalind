7a43099a6284b3b695ba51ee53dbc4a3bf48e0ce
chl <chl@aliquote.org>
2020-01-24 14:49:52 +0100

    ↻ wip

---
+++ bioinfo/trie.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from itertools import count
+
+DEBUG = True
+
+
+class Trie:
+    """A simple trie implementation"""
+    def __init__(self):
+        self.counter = count(start=1)
+        self.root = [next(self.counter),{}]
+
+    def insert(self, sequence):
+        node = self.root
+        for ch in sequence:
+            if ch not in node[1]:
+                node[1][ch] = [next(self.counter),{}]
+            node = node[1][ch]
+
+
+with open("trie.txt") as fh:
+    data = fh.readlines()
+
+ss = [line.strip() for line in data]
+tt = Trie()
+for s in ss:
+    tt.insert(s)
+
+if DEBUG:
+    print(tt.root)
+
+
+
+
+
+
